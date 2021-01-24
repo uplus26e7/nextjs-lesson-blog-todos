5ca91e5eee8da9abc3de3579ad6bf4e75e3a5048
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2021-01-24 17:52:52 +0900

    🚧 Main page

--- pages/main-page.js
+++ pages/main-page.js
@@ -2,6 +2,7 @@ import { Router } from 'next/router';
 import Cookie from 'universal-cookie';
 import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
+import Link from 'next/link';
 
 const cookie = new Cookie();
 
@@ -14,6 +15,18 @@ export default function MainPage() {
 
   return (
     <Layout title="Main page">
+      <div className="mb-10">
+        <Link href="/blog-page">
+          <a className="bg-indigo-500 mr-8 hover:bg-indigo-600 text-white px-4 py-12 rounded">
+            Visit Blog by SSG + ISR
+          </a>
+        </Link>
+        <Link href="/task-page">
+          <a className="bg-indigo-500 mr-8 hover:bg-indigo-600 text-white px-4 py-12 rounded">
+            Visit Task by ISR + CSR
+          </a>
+        </Link>
+      </div>
       <svg
         onClick={logout}
         class="mt-10 cursor-pointer w-6 h-6"
