3c23929b4b445823b57739f34b2e1fecb281a6bb
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2021-01-24 19:12:01 +0900

    🚧 ISR

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,9 +1,15 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import { getAllPostsData } from '../lib/posts';
+import Post from '../components/Post';
 
-export default function BlogPage() {
+export default function BlogPage({ filteredPosts }) {
   return (
     <Layout title="Blog page">
+      <ul>
+        {filteredPosts &&
+          filteredPosts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
       <Link href="/main-page">
         <div className="flex cursor-pointer mt-12">
           <svg
@@ -25,4 +31,14 @@ export default function BlogPage() {
       </Link>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const filteredPosts = await getAllPostsData();
+  return {
+    props: {
+      filteredPosts,
+    },
+    revalidate: 3,
+  };
 }
